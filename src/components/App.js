376590b8e84df0ea13bb1921747785a2399c7156
kamrankhan0001 <kamranbgp15@gmail.com>
2023-07-24 13:29:14 +0530

    submitting code-Mon Jul 24 2023 13:29:13 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,35 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+const[name, setName]=useState("");
+const[email, setEmail]=useState("");
+const[password, setPassword]=useState("");
+
+function handleClick(){
+setName(name,email,password)
+}
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <form onSubmit={handleClick}>
+      <div>
+        <label>Name</label>
+        <input type="text"/>
+        </div>
+
+        <div>
+        <label>Email</label>
+        <input type="email"/>
+        </div>
+        
+        <div>
+        <label>Password</label>
+        <input type="password"/>
+        </div>
+        </form>
+        <button onClick={handleClick}>Submit</button>
     </div>
   )
 }
